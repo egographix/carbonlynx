3f7703e9d27a07eaa1283604ef9138d68925dd9f
John Warren <egographix@earthlink.net>
2020-03-03 20:12:01 -0500

    Webapp bundle

---
+++ webapp/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	mode: "development",
+	entry: "./src/index.ts",
+		output: { filename: "bundle.js" }, resolve: { extensions: [".ts", ".js"] },
+	module: {
+		rules: [ { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ } ]
+	}
+;
