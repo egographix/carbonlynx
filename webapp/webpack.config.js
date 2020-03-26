0bcfa100fab244bb877299a1352ba962a7631432
John Warren <egographix@earthlink.net>
2020-03-26 14:35:53 -0400

    add html file and change config to look in assets folder

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -4,5 +4,8 @@ module.exports = {
 		output: { filename: "bundle.js" }, resolve: { extensions: [".ts", ".js"] },
 	module: {
 		rules: [ { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ } ]
+	},	
+	devServer: {
+		contentBase: "./assets", port: 4500
 	}
 };
